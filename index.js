0cd1d9688e7812d7c22ea87b908f7304091ab796
dkc120 <dilipcyp120A@gmail.com>
2023-12-25 00:05:40 +0530

    added new

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(
     allowedHeaders: ["Content-Type", "X-Auth-Token", "Origin", "Authorization"],
   })
 );
-
+console.log(cors, "hellow cors");
 app.use(express.json());
 app.use(cookieParser());
 
